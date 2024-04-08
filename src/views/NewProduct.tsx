d0eadd848a40ab1c4f03dd11ee880d8d0ee6bea6
Franco Silva <franco_tigres18@hotmail.com>
2024-04-08 09:33:26 -0600

    feat:(Componente Formulario reutilizable)

--- src/views/NewProduct.tsx
+++ src/views/NewProduct.tsx
@@ -3,10 +3,11 @@ import {
   Form,
   useActionData,
   ActionFunctionArgs,
-  redirect
+  redirect,
 } from 'react-router-dom';
 import ErrorMessage from '../components/ErrorMessage';
 import { addProduct } from '../services/ProductService';
+import ProductForm from '../components/ProductForm';
 
 export async function action({ request }: ActionFunctionArgs) {
   const data = Object.fromEntries(await request.formData());
@@ -19,14 +20,13 @@ export async function action({ request }: ActionFunctionArgs) {
     return error;
   }
 
-  await addProduct(data)
-  
+  await addProduct(data);
+
   return redirect('/');
 }
 
 export default function newProduct() {
   const error = useActionData() as string;
-  console.log(error);
 
   return (
     <>
@@ -48,36 +48,9 @@ export default function newProduct() {
         method="POST"
         className="mt-10"
       >
-        <div className="mb-4">
-          <label
-            className="text-gray-800"
-            htmlFor="name"
-          >
-            Nombre Producto:
-          </label>
-          <input
-            id="name"
-            type="text"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Nombre del Producto"
-            name="name"
-          />
-        </div>
-        <div className="mb-4">
-          <label
-            className="text-gray-800"
-            htmlFor="price"
-          >
-            Precio:
-          </label>
-          <input
-            id="price"
-            type="number"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Precio Producto. ej. 200, 300"
-            name="price"
-          />
-        </div>
+
+        <ProductForm />
+
         <input
           type="submit"
           className="mt-5 w-full bg-indigo-600 p-2 text-white font-bold text-lg cursor-pointer rounded"
