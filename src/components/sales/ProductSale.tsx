9c90850a8d0358e961b8a8e6ee16655b78722c7d
Franco Silva <franco_tigres18@hotmail.com>
2024-04-26 14:55:24 -0600

    feat(Component ProductSale):Reduccion de padding

--- src/components/sales/ProductSale.tsx
+++ src/components/sales/ProductSale.tsx
@@ -9,7 +9,7 @@ type ProductSaleProps = {
 export default function ProductSale({ item, addItem }: ProductSaleProps) {
   return (
     <button
-      className="border-2 border-blue-400 w-full p-3 rounded-lg hover:bg-blue-400"
+      className="border-2 border-blue-400 w-full p-2 rounded-lg hover:bg-blue-400"
       onClick={() => addItem(item)}
     >
       <p className="text-xl">{item.name}</p>
