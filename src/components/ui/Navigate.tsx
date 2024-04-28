d4b8cd4fb7fdf99d2db0d0ac235eaa803034f3dc
Franco Silva <franco_tigres18@hotmail.com>
2024-04-28 09:40:06 -0600

    feat(Layouts): Navegacion agregadas

---
+++ src/components/ui/Navigate.tsx
@@ -0,0 +1,20 @@
+import { Link } from 'react-router-dom';
+
+export default function Navigate() {
+  return (
+    <div className='flex gap-3'>
+      <Link
+        to="/"
+        className="text-white text-xl font-bold p-2 hover:border hover:border-white"
+      >
+        Productos
+      </Link>
+      <Link
+        to="/ventas"
+        className="text-white text-xl font-bold p-2 hover:border hover:border-white"
+      >
+        Ventas
+      </Link>
+    </div>
+  );
+}
--- src/layouts/LayoutSales.tsx
+++ src/layouts/LayoutSales.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom';
+import Navigate from '../components/ui/Navigate';
 
 export default function LayoutSales() {
   return (
     <>
-      <header className="bg-slate-800">
-        <div className="mx-auto max-w-6xl py-10">
+      <header className="bg-slate-800 flex justify-around items-center">
+        <div className="max-w-6xl py-10">
           <h1 className="text-4xl font-extrabold text-white">
             Venta de Productos
           </h1>
         </div>
+        <Navigate />
       </header>
 
       <main className="mt-10 mx-auto max-w-6xl  p-10 bg-white shadow">
